1e5f58518d6e118a46fe80971af740a7e1ba16cc
Anshul Chaudhary <56474712+Anshul-05@users.noreply.github.com>
2021-04-03 12:25:07 +0530

    Update Mat_exp.cpp

--- Maths/Mat_exp.cpp
+++ Maths/Mat_exp.cpp
@@ -1,18 +1,24 @@
 #define K 15
+
 struct mat{
 	int data[K][K];
 };
+
 mat operator*(mat &a, mat &b){
 	mat c;
-	rep(i,0,K)rep(j,0,K){
+	for(int i=0;i<K;++i)for(int j=0;j<K;++j){
 		c.data[i][j]=0;
-		rep(k,0,K)(c.data[i][j]+=((a.data[i][k]%md*b.data[k][j]%md))%md)%=md;
+		for(int k=0;k<K;++k) {
+			c.data[i][j]+=(a.data[i][k]*1ll*b.data[k][j])%md;
+			if(c.data[i][j]>=md) c.data[i][j]-=md;
+		}
 	}
 	return c;
 }
+
 mat operator^(mat &a,int b){
 	mat c;
-	rep(i,0,K)rep(j,0,K)c.data[i][j]=(i==j);
+	for(int i=0;i<K;++i)for(int j=0;j<K;++j) c.data[i][j]=(i==j);
 	while(b){if(b&1)c=c*a;a=a*a;b/=2;}
 	return c;
 }
