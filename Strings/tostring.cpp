ad1345fff0839c8629be70bf6f5a2b2dafcab784
Anshul Chaudhary <56474712+Anshul-05@users.noreply.github.com>
2021-04-03 12:29:02 +0530

    Update tostring.cpp

--- Strings/tostring.cpp
+++ Strings/tostring.cpp
@@ -1,7 +1,7 @@
-string tostring(int n) {
-	if(!n) return "0";
+string tostring(int n,int k=1) {
 	string ans;
 	while(n) ans+=(char)(n%2+'0'),n/=2;
+	while(sz(ans)<k) ans+="0";
 	reverse(all(ans));
 	return ans;
 }
