38d1607fe6e80b35ec2210973670bcc1335642a1
Anshul Chaudhary <56474712+Anshul-05@users.noreply.github.com>
2021-04-05 20:28:39 +0530

    Add files via upload

---
+++ Flow/Dinic.cpp
@@ -0,0 +1,68 @@
+struct Edge {
+	int u,v,cap,flow=0;
+	Edge(int u,int v,int cap) : u(u), v(v), cap(cap) {}
+};
+
+class Dinic {
+	public:
+	vector<Edge> edges;
+	vector<vector<int>> G;
+	int n,m=0,s,t;
+	vector<int> H,ptr;
+	queue<int> q;
+	
+	Dinic(int n,int s,int t) : n(n), s(s), t(t) {
+		G.resize(n+1);
+		H=ptr=vector<int>(n+1);
+	}
+	
+	void add(int u,int v,int cap) {
+		edges.emplace_back(u,v,cap);
+		edges.emplace_back(v,u,0);
+		G[u].push_back(m++);
+		G[v].push_back(m++);
+	}
+	
+	bool bfs() {
+		while(!q.empty()) {
+			int u=q.front();
+			q.pop();
+			for(int v:G[u]) {
+				int to=edges[v].v;
+				int gap=edges[v].cap-edges[v].flow;
+				if(H[to]!=-1 or gap<1) continue;
+				H[to]=H[u]+1,q.push(to);
+			}
+		}
+		return H[t]!=-1;
+	}
+	
+	int dfs(int u,int X) {
+		if(!X) return 0;
+		if(u==t) return X;
+		for(int &i=ptr[u];i<sz(G[u]);++i) {
+			int v=G[u][i],to=edges[v].v;
+			int gap=edges[v].cap-edges[v].flow;
+			if(H[to]!=(H[u]+1) or gap<1) continue;
+			int Y=dfs(to,min(X,gap));
+			if(!Y) continue;
+			edges[v].flow+=Y;
+			edges[v^1].flow-=Y;
+			return Y;
+		}
+		return 0;
+	}
+	
+	int flow() {
+		int ans=0;
+		while(true) {
+			fill(H.begin(),H.end(),-1);
+			H[s]=0,q.push(s);
+			if(!bfs()) break;
+			fill(ptr.begin(),ptr.end(),0);
+			while(int X=dfs(s,inf)) ans+=X;
+		}
+		return ans;
+	}
+};
+// EV^2  for unit network- Esqrt(V)
---
+++ Flow/matching.cpp
@@ -0,0 +1,50 @@
+class matching {
+	public:
+	vector<vector<int>> G;
+	vector<int> pa,pb,was;
+	int n,m,res,t;
+	
+	matching(int n,int m) : n(n), m(m) {
+		pa=vector<int>(n+1,-1);
+		pb=vector<int>(m+1,-1);
+		was=vector<int>(n+1,0);
+		G.resize(n+1);
+		res=t=0;
+	}
+	
+	void add(int u,int v) {
+		G[u].push_back(v);
+	}
+	
+	bool dfs(int u) {
+		was[u]=t;
+		for(int v:G[u])
+			if(pb[v]<0) {
+				pb[v]=u,pa[u]=v;
+				return true;
+			}
+		for(int v:G[u]) 
+			if(was[pb[v]]!=t and dfs(pb[v])) {
+				pb[v]=u,pa[u]=v;
+				return true;
+			}
+		return false;
+	}
+	
+	int solve() {
+		while(true) {
+			t++;
+			int add=0;
+			for(int i=0;i<n;++i) 
+				if(pa[i]<0 and dfs(i)) ++add;
+			if(!add) break;
+			res+=add;
+		}
+		return res;
+	}
+	bool run_one(int u) {
+		if(pa[u]<0) return 0;
+		t++;
+		return dfs(v);
+	}
+};
